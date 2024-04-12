909112e28878f38e74fca46fb8a05e35e5d1cf5f
Evgenii Kulshan <eikulshan@gmail.com>
2024-04-12 14:06:20 +0300

    add category entity

---
+++ app/controllers/web/categories_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Web::CategoriesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     delete 'auth', to: 'auth#destroy', as: :auth_destroy
+
+    resource :categories
   end
 end

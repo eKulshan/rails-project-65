bc5ef311721802a75c8ac3ecd090874cb42df372
Evgenii Kulshan <evgenii.k@devstark.com>
2024-05-05 21:49:03 +0300

    used add admin field

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@
 # Table name: users
 #
 #  id         :integer          not null, primary key
+#  admin      :boolean          default(FALSE)
 #  email      :string
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_admin  (admin)
 #
 class User < ApplicationRecord
   has_many :bulletins, dependent: :destroy
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,15 @@
 # Table name: users
 #
 #  id         :integer          not null, primary key
+#  admin      :boolean          default(FALSE)
 #  email      :string
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_admin  (admin)
 #
 require 'test_helper'
 
